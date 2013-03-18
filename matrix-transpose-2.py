6b43980b5f219bce33c68586e9b83ef528c5588f
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2013-03-18 20:22:06 +0100

    Use list comprehension instead of a map to avoid an additional list() with Python 3.

--- matrix-transpose-2.py
+++ matrix-transpose-2.py
@@ -19,12 +19,10 @@ Output: transposed matrix tA
 def checkio(matr):
     'return a transposed matrix'
     l = len(matr[0])
-    return map(lambda i: sum(matr, [])[i::l], range(l))
+    return [sum(matr, [])[i::l] for i in range(l)]
     
 if __name__ == '__main__':
-    assert checkio([[1,2],
-             [1,2]]) ==  [[1, 1],
-                          [2, 2]], 'First'
+    assert checkio([[1,2], [1,2]]) ==  [[1, 1], [2, 2]], 'First'
     assert checkio([[1,0,3,4,0],
                     [2,0,4,5,6],
                     [3,4,9,0,6]]) == [[1, 2, 3],
@@ -32,4 +30,4 @@ if __name__ == '__main__':
                                       [3, 4, 9],
                                       [4, 5, 0],
                                       [0, 6, 6]],'Second'
-    print 'All ok'
+    print('All ok')
