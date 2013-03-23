80568ecfae8177d8dfd40267308db07645a50d08
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2013-03-23 21:25:56 +0100

    Make matrix transpose code pep8 compliant

--- matrix-transpose.py
+++ matrix-transpose.py
@@ -16,18 +16,19 @@ Input: matrix A
 Output: transposed matrix tA
 """
 
+
 def checkio(matr):
     'return a transposed matrix'
     l = len(matr[0])
     return [sum(matr, [])[i::l] for i in range(l)]
-    
+
 if __name__ == '__main__':
-    assert checkio([[1,2], [1,2]]) ==  [[1, 1], [2, 2]], 'First'
-    assert checkio([[1,0,3,4,0],
-                    [2,0,4,5,6],
-                    [3,4,9,0,6]]) == [[1, 2, 3],
-                                      [0, 0, 4],
-                                      [3, 4, 9],
-                                      [4, 5, 0],
-                                      [0, 6, 6]],'Second'
+    assert checkio([[1, 2], [1, 2]]) == [[1, 1], [2, 2]], 'First'
+    assert checkio([[1, 0, 3, 4, 0],
+                    [2, 0, 4, 5, 6],
+                    [3, 4, 9, 0, 6]]) == [[1, 2, 3],
+                                          [0, 0, 4],
+                                          [3, 4, 9],
+                                          [4, 5, 0],
+                                          [0, 6, 6]], 'Second'
     print('All ok')
